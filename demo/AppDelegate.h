d1efb01cecd26da74d068d31b90ad6273aa232c3
沫苓小怪 <shuzhenguovip@163.com>
2016-11-16 20:46:04 +0800

    Initial Commit

---
+++ demo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  demo
+//
+//  Created by shuzhenguo on 2016/11/16.
+//  Copyright © 2016年 shuzhenguo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ demo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  demo
+//
+//  Created by shuzhenguo on 2016/11/16.
+//  Copyright © 2016年 shuzhenguo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
