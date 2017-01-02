498b73faa3b139ec28f10da2c750ddb57ff1de3b
shuzhenguo <shuzhenguovip@163.com>
2017-01-02 19:36:37 +0800

    绘图之圆角

--- demo/ViewController.m
+++ demo/ViewController.m
@@ -17,6 +17,19 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    
+    UIView *view2 = [[UIView alloc] initWithFrame:CGRectMake(120, 10, 80, 80)];
+    view2.backgroundColor = [UIColor redColor];
+    [self.view addSubview:view2];
+    
+    UIBezierPath *maskPath = [UIBezierPath bezierPathWithRoundedRect:view2.bounds byRoundingCorners:UIRectCornerTopLeft | UIRectCornerTopRight cornerRadii:CGSizeMake(10, 10)];
+    
+    
+    CAShapeLayer *maskLayer = [[CAShapeLayer alloc] init];
+    maskLayer.frame = view2.bounds;
+    maskLayer.path = maskPath.CGPath;
+    view2.layer.mask = maskLayer;
+
 }
 
 
